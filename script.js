c619e057ea544d45911ff8ae369067c72eec7fb8
avverma9 <av95766@gmail.com>
2022-12-30 18:32:43 +0530

    updated code

--- script.js
+++ script.js
@@ -10,16 +10,16 @@ let masterSongName = document.getElementById('masterSongName');
 let songItems = Array.from(document.getElementsByClassName('songItem'));
 
 let songs = [
-    {songName: "Warriyo - Mortals [NCS Release]", filePath: "songs/1.mp3", coverPath: "covers/1.jpg"},
-    {songName: "Cielo - Huma-Huma", filePath: "songs/2.mp3", coverPath: "covers/2.jpg"},
-    {songName: "DEAF KEV - Invincible [NCS Release]-320k", filePath: "songs/3.mp3", coverPath: "covers/3.jpg"},
-    {songName: "Different Heaven & EH!DE - My Heart [NCS Release]", filePath: "songs/4.mp3", coverPath: "covers/4.jpg"},
-    {songName: "Janji-Heroes-Tonight-feat-Johnning-NCS-Release", filePath: "songs/5.mp3", coverPath: "covers/5.jpg"},
-    {songName: "Rabba - Salam-e-Ishq", filePath: "songs/2.mp3", coverPath: "covers/6.jpg"},
-    {songName: "Sakhiyaan - Salam-e-Ishq", filePath: "songs/2.mp3", coverPath: "covers/7.jpg"},
-    {songName: "Bhula Dena - Salam-e-Ishq", filePath: "songs/2.mp3", coverPath: "covers/8.jpg"},
-    {songName: "Tumhari Kasam - Salam-e-Ishq", filePath: "songs/2.mp3", coverPath: "covers/9.jpg"},
-    {songName: "Na Jaana - Salam-e-Ishq", filePath: "songs/4.mp3", coverPath: "covers/10.jpg"},
+    {songName: "Teri Hogaiyan 2 - Vishal Mishra", filePath: "songs/1.mp3", coverPath: "covers/1.jpg"},
+    {songName: "Tera yaar Hoon Mai - Arijit SIngh", filePath: "songs/2.mp3", coverPath: "covers/2.jpg"},
+    {songName: "Pika Pika Chu - Pikchu By - Prachi Sharma", filePath: "songs/3.mp3", coverPath: "covers/3.jpg"},
+    {songName: "Udaariyaan - Satinder Sartaaj", filePath: "songs/4.mp3", coverPath: "covers/4.jpg"},
+    {songName: "Sabaat OST - Sabaat", filePath: "songs/5.mp3", coverPath: "covers/5.jpg"},
+    {songName: "Heer Ranjha - Rito Riba", filePath: "songs/6.mp3", coverPath: "covers/6.jpg"},
+    {songName: "Calm Down - English Song", filePath: "songs/7.mp3", coverPath: "covers/7.jpg"},
+    {songName: "Distance Love - Zehr Vibe,Grewal", filePath: "songs/8.mp3", coverPath: "covers/8.jpg"},
+    {songName: "Cold Hours - Aleem Rk", filePath: "songs/9.mp3", coverPath: "covers/9.jpg"},
+    {songName: "Bad Habbits - Ed Sheeran", filePath: "songs/10.mp3", coverPath: "covers/10.jpg"},
 ]
 
 songItems.forEach((element, i)=>{ 
